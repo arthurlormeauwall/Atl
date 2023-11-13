036c7188853173a88844d4aacd49ccf446274d3e
arthurlormeauwall <arthurlormeau@gmail.com>
2023-11-13 15:31:12 +0100

    remove noise and clean

--- sample_project_test/test_module/src/MyTestClass.cpp
+++ sample_project_test/test_module/src/MyTestClass.cpp
@@ -2,28 +2,15 @@
 #include "MyClass.h"
 #include "atl_includes.h"
 
-struct o : public Comparable{
-	int a = 3;
-	string toString() {
-		return std::to_string(a);
-	}
-	bool operator==(const Comparable* rhs) override {
-		return a == ((o*)rhs)->a;
-	}
-	o(int p) : a(p) {}
-};
-
-
 void MyTestClass::addChildren() {
 	add(createUnitTestBuilder("should add two number",
 		[]()->std::vector<Result>
 		{
 			Assertions assertions;
-			o a(4);
-			o b(5);
-			Comparable* actual = &a;
-			Comparable* expected = &b;
-			assertions.push_back(assertThat<Comparable*>(actual).isEqualTo(expected)->getResult());
+			MyClass<float> obj;
+			float expected = 5.;
+			float actual = obj.add(2., 3.);
+			assertions.push_back(assertThat<float>(actual).isEqualTo(expected)->getResult());
 			return assertions;
 		}
 	));
