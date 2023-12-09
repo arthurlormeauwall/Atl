5f1eba98d60399a87facd7b1dc57f33c2d72ba5a
arthurlormeauwall <arthurlormeau@gmail.com>
2023-12-09 12:36:40 +0100

    rebase

---
+++ sample_project_test/test_module/src/ItemTest.h
@@ -0,0 +1,25 @@
+#pragma once
+#include "atl_includes.h"
+
+struct Item {
+	string name;
+	int value;
+	bool operator==(Item& rhs) {
+		return name == rhs.name && value == rhs.value;
+	}
+	string(*toString)(Item it) =
+		[](Item it)->string { return string("Name : ")
+		.append(it.name)
+		.append(" and value : ")
+		.append(std::to_string(it.value));
+	};
+
+	Item(string n, int v) :name(n), value(v) {}
+};
+
+
+class ItemTest: public TestClassBuilder {
+public:
+	ItemTest(TestData td) : TestClassBuilder(td) {}
+	virtual void addUnitTests() override;
+};
