15ccf0423f553da60073cbb5a033c9e567899639
arthurlormeauwall <arthurlormeau.adm@gmail.com>
2024-10-01 21:58:56 +0200

    format last details

--- example/atlRunner/src/main.cpp
+++ example/atlRunner/src/main.cpp
@@ -4,6 +4,4 @@ int main(int argc, char* argv[]) {
 	auto atlController = AtlController(argv);;
 	atlController.runAllTests(std::make_shared<MyTests>());
 	std::cin.get();
-	// std::cout<<"Hello from atl app"<<std::endl;
-	
 }
