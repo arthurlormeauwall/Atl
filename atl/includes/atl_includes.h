0ad647aaaddae1120c942c02390cc6cc45753dfe
arthurlormeauwall <arthurlormeau.adm@gmail.com>
2024-10-02 12:34:23 +0200

    work great

---
+++ atl/includes/atl_includes.h
@@ -0,0 +1,6 @@
+#pragma once
+#include "../src/template.h"
+#include "../src/controller/AtlController.h"
+#include "../src/service/assertion/IsEqualTo.h"
+#include "../src/out/consoleOutput/ConsoleOutputWriter.h"
+#include "../src/testBuilder/TestBuilder.h"
--- build.sh
+++
@@ -1,3 +0,0 @@
-cmake -S . -B build/ ; cmake --build build ; 
-cp build/atl/atlCore/libatlCore.a lib/atl.a;
-cp build/atl/atlRunner/atlRunner example/testRunner/atlRunner
