9bc8ed762a0004317132714767830a587a01443a
arthurlormeauwall <arthurlormeau.adm@gmail.com>
2024-10-02 13:21:50 +0200

    remove unused include

--- example/test/atlRunner/src/main.cpp
+++ example/test/atlRunner/src/main.cpp
@@ -1,5 +1,4 @@
 #include "AllTests.h"
-#include <iostream>
 int main(int argc, char* argv[]) {
 	auto atlController = AtlController(argv);;
 	atlController.runAllTests(std::make_shared<AllTests>());
